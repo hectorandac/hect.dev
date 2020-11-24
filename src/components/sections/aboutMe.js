bc50c1c3f77b42cf57b6b4275ca274d2cfee4cb4
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2020-11-24 00:01:08 -0400

    Added About section, Closes #12

--- src/components/sections/aboutMe.js
+++ src/components/sections/aboutMe.js
@@ -59,7 +59,7 @@ export default function AboutMe() {
         <TextContainer>
             <p>I'm a software engineer at the Dominican Republic <span role="img" aria-label="DR" style={{color: 'white'}}>🇩🇴</span>, graduated from <a href="https://www.intec.edu.do/">INTEC University</a>. I also studied for a semester in <a href="https://eng.sejong.ac.kr/index.do">Sejong University</a> at Korea <span role="img" aria-label="KO" style={{color: 'white'}}>🇰🇷</span>.</p>
             {/* <p>I've been programming and managing small teams for the creation of amazing software projects for about {(new Date()).getFullYear() - 2014} years, with perfomance and scalability in mind.</p> */}
-            <p>At the moment I'm working with <a href="https://mgage.com/">mGage</a>, a global organization with multiple interesting projects that goes from mobile messaging to payments solutions</p>
+            <p>At the moment I'm working with <a href="https://mgage.com/">mGage</a>, a global organization with multiple interesting projects that goes from mobile messaging to payments solutions.</p>
             <p>These are some of the technologies I've been working with:</p>
             <CodeList>
                 <IconContext.Provider value={{ color: "#FFC247", size: "16px"}}>
