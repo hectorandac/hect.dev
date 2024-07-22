f364a481a14e181649d52b0d3a4d61e2422e5018
Hector Andres Acosta Pozo <hector.acosta@betterup.co>
2024-07-22 17:10:23 +0900

    Additional updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: `hect.dev`,
-    description: `I'm a software engineer residing in the Dominican Republic 🏝. For the past 6 years I've been creating all sorts of applications, ranging from complex backend solutions to beautifully designed websites.`,
-    author: `@hectorandac`,
+    title: 'hect.dev',
+    description: 'I\'m a software engineer residing in the Dominican Republic 🏝. For the past 6 years I\'ve been creating all sorts of applications, ranging from complex backend solutions to beautifully designed websites.',
+    author: '@hectorandac',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-styled-components`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `hect.dev`,
-        start_url: `/`,
-        background_color: `#092934`,
-        theme_color: `#092934`,
-        display: `minimal-ui`,
-        icon: `src/images/logo_favicon.svg`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'hect.dev',
+        start_url: '/',
+        background_color: '#092934',
+        theme_color: '#092934',
+        display: 'minimal-ui',
+        icon: 'src/images/logo_favicon.svg', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
 
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
